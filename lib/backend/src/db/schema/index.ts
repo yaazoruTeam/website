1ff02f2f115e1b70f7446e65fa9200f88e9e5940
Yaki Klein <49128330+klakdev@users.noreply.github.com>
2024-12-03 10:09:48 +0200

    Revert "add creat schema to device and Transferring the check for whether schema-yaaoru consent exists to a separate file"
    This reverts commit bf7bdc77dc8c0adef176488887baae3dc028ede3.

--- lib/backend/src/db/schema/index.ts
+++ lib/backend/src/db/schema/index.ts
@@ -1,14 +1,9 @@
-import { createYaazoruSchema } from "./yaazoru"
 import { createCustomerSchema } from "./customer"
-import { createDeviceSchema } from "./device"
 
-
-const createSchema = async () => {
+const createSchema = async() => {
     console.log("Creating schema...");
     try {
-        await createYaazoruSchema();
         await createCustomerSchema();
-        await createDeviceSchema();
         console.log("Schema created successfully");
     } catch (err) {
         console.error("Error creating schema", err);
