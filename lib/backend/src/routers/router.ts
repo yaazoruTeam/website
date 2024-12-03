2b71098e7b77fab34bba876266eb431b9fc9135c
Efrat Grinboim <151626406+Efrat6821@users.noreply.github.com>
2024-12-03 12:08:17 +0200

    add router to customerDevice

--- lib/backend/src/routers/router.ts
+++ lib/backend/src/routers/router.ts
@@ -1,6 +1,7 @@
 import { Router, ErrorRequestHandler } from 'express';
 import * as customersController from '../controller/customer';
 import * as devicesController from '../controller/device';
+import * as customerDevicesController from '../controller/customerDevice';
 import { errorHandler } from 'src/errorHandler';
 
 
@@ -18,6 +19,12 @@ router.get(`${ROUTE_PATH}/device`, devicesController.getDevices);
 router.get(`${ROUTE_PATH}/device/:id`, devicesController.getDeviceById);
 router.put(`${ROUTE_PATH}/device/:id`, devicesController.updateDevice);
 router.delete(`${ROUTE_PATH}/device/:id`, devicesController.deleteDevice);
+
+router.post(`${ROUTE_PATH}/customerDevice`, customerDevicesController.createCustomerDevice);
+router.get(`${ROUTE_PATH}/customerDevice`, customerDevicesController.getCustomersDevices);
+router.get(`${ROUTE_PATH}/customerDevice/:id`, customerDevicesController.getCustomerDeviceById);
+router.put(`${ROUTE_PATH}/customerDevice/:id`, customerDevicesController.updateCustomerDevice);
+router.delete(`${ROUTE_PATH}/customerDevice/:id`, customerDevicesController.deleteCustomerDevice);
 
 
 router.all('*', errorHandler)
