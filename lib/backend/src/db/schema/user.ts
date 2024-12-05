4c9daa2be9ce3e2f3bace06b866a0d518deae3bc
Efrat Grinboim <151626406+Efrat6821@users.noreply.github.com>
2024-12-05 17:24:24 +0200

    add middleware to auth admin/branch

--- lib/backend/src/db/schema/user.ts
+++ lib/backend/src/db/schema/user.ts
@@ -23,7 +23,7 @@ const createUserSchema = async () => {
                 table.string("zipCode").notNullable();
                 table.string("password").notNullable().unique();
                 table.string("user_name").notNullable().unique();
-                table.string("role").notNullable();
+                table.enu("role", ["admin", "branch"]).notNullable();
             });
             console.log('User table created successfully.');
         }
