1c5f2e99b9b4421cffa8ec8eebf7db957ab3f8a6
Efrat Grinboim <151626406+Efrat6821@users.noreply.github.com>
2024-12-15 10:57:38 +0200

    ✨add actions to DB for branchCustomer and branchUser

---
+++ lib/backend/src/db/BranchCustomer.ts
@@ -0,0 +1,89 @@
+import { BranchCustomer } from "@yaazoru/model";
+import getConnection from "./connection";
+
+
+
+const createBranchCustomer = async (branchCustomer: BranchCustomer.Model) => {
+    const knex = getConnection();
+    try {
+        const [newBranchCustomer] = await knex('yaazoru.branchCustomer')
+            .insert({
+                branch_id: branchCustomer.branch_id,
+                customer_id: branchCustomer.customer_id,
+            }).returning('*');
+        return newBranchCustomer;
+    }
+    catch (err) {
+        throw err;
+    };
+}
+
+const getAllBranchCustomer = async (): Promise<BranchCustomer.Model[]> => {
+    const knex = getConnection();
+    try {
+        return await knex.select().table('yaazoru.branchCustomer');
+    }
+    catch (err) {
+        throw err;
+    };
+}
+
+const getBranchCustomerById = async (branchCustomer_id: string) => {
+    const knex = getConnection();
+    try {
+        return await knex('yaazoru.branchCustomer').where({ branchCustomer_id }).first();
+    } catch (err) {
+        throw err;
+    };
+};
+
+const updateBranchCustomer = async (branchCustomer_id: string, branchCustomer: BranchCustomer.Model) => {
+    const knex = getConnection();
+    try {
+        const updateBranchCustomer = await knex('yaazoru.branchcustomer')
+            .where({ branchCustomer_id })
+            .update(branchCustomer)
+            .returning('*');
+        if (updateBranchCustomer.length === 0) {
+            throw { status: 404, message: 'branchCustomer not found' };
+        }
+        return updateBranchCustomer[0];
+    } catch (err) {
+        throw err;
+    };
+};
+
+const deleteBranchCustomer = async (branchCustomer_id: string) => {
+    const knex = getConnection();
+    try {
+        const deleteBranchCustomer = await knex('yaazoru.branchCustomer').where({ branchCustomer_id }).del().returning('*');
+        if (deleteBranchCustomer.length === 0) {
+            throw { status: 404, message: 'BranchCustomer not found' };
+        }
+        return deleteBranchCustomer[0];
+    } catch (err) {
+        throw err;
+    };
+};
+
+const doesBranchCustomerExist = async (branchCustomer_id: string): Promise<boolean> => {
+    const knex = getConnection();
+    try {
+        const result = await knex('yaazoru.branchCustomer')
+            .select('branchCustomer_id')
+            .where({ branchCustomer_id })
+            .first();
+        return !!result;
+    } catch (err) {
+        throw err;
+    }
+};
+
+export {
+    createBranchCustomer,
+    getAllBranchCustomer,
+    getBranchCustomerById,
+    updateBranchCustomer,
+    deleteBranchCustomer,
+    doesBranchCustomerExist,
+}
--- lib/backend/src/db/index.ts
+++ lib/backend/src/db/index.ts
@@ -4,6 +4,8 @@ import * as Device from "./Device";
 import * as CustomerDevice from "./CustomerDevice";
 import * as User from "./User";
 import * as Branch from "./Branch";
+import * as BranchCustomer from "./BranchCustomer";
+import * as BranchUser from "./BranchUser";
 
 export default {
     Customer,
@@ -11,4 +13,6 @@ export default {
     CustomerDevice,
     User,
     Branch,
+    BranchCustomer,
+    BranchUser,
 }
