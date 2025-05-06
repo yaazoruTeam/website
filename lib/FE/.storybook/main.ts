33f9290d053ddac509afc1bf3e1c9b03d9fde843
Efrat Grinboim <151626406+Efrat6821@users.noreply.github.com>
2025-05-06 12:44:26 +0300

    ⚡ Reinitialized Storybook after migrating the project to Vite
    - Reinstalled and configured Storybook to work with Vite
    - Updated Storybook dependencies to support the new build system
    - Verified compatibility with existing components and stories

--- lib/FE/.storybook/main.ts
+++ lib/FE/.storybook/main.ts
@@ -1,18 +1,19 @@
-import type { StorybookConfig } from "@storybook/react-webpack5";
+import type { StorybookConfig } from '@storybook/react-vite';
 
 const config: StorybookConfig = {
-  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
-  addons: [
-    "@storybook/preset-create-react-app",
-    "@storybook/addon-onboarding",
-    "@storybook/addon-essentials",
-    "@chromatic-com/storybook",
-    "@storybook/addon-interactions",
+  "stories": [
+    "../src/**/*.mdx",
+    "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"
   ],
-  framework: {
-    name: "@storybook/react-webpack5",
-    options: {},
-  },
-  staticDirs: ["..\\public"],
+  "addons": [
+    "@storybook/addon-essentials",
+    "@storybook/addon-onboarding",
+    "@chromatic-com/storybook",
+    "@storybook/experimental-addon-test"
+  ],
+  "framework": {
+    "name": "@storybook/react-vite",
+    "options": {}
+  }
 };
 export default config;
