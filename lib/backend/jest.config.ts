028d9e26ad5da259c3e6fda46692d4e9ae8d207c
michal22362 <michal22362@gmail.com>
2025-05-07 14:57:40 +0300

    🔧 Added mock for console.log, console.warn, and console.error during test runs

--- lib/backend/jest.config.ts
+++ lib/backend/jest.config.ts
@@ -3,6 +3,7 @@ import type { Config } from 'jest';
 const config: Config = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  setupFiles: ["<rootDir>/tests/jest.setup.ts"],
   testMatch: ['**/tests/**/*.test.ts'],
   moduleFileExtensions: ['ts', 'js'],
 };
