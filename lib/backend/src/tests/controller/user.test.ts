f92aee047cf94497d041201cba6d14b314bc4dcf
michal22362 <michal22362@gmail.com>
2025-05-12 18:37:24 +0300

    ✅ Adding unit tests to a user controller

--- lib/backend/src/tests/controller/user.test.ts
+++ lib/backend/src/tests/controller/user.test.ts
@@ -1,13 +1,13 @@
 import request from 'supertest';
 import express from 'express';
-import { createUser, getUsers, getUserById, updateUser, deleteUser } from '../controller/user';
-import * as db from '../../src/db';
-import { User } from '../model';
-import { hashPassword } from '../utils/password';
+import { createUser, getUsers, getUserById, updateUser, deleteUser } from '../../controller/user';
+import * as db from '../../db';
+import { User } from '../../model/src';
+import { hashPassword } from '../../utils/password';
 
-jest.mock('../db');
-jest.mock('../utils/password');
-jest.mock('../model');
+jest.mock('../../db');
+jest.mock('../../utils/password');
+jest.mock('../../model');
 
 const app = express();
 app.use(express.json());
