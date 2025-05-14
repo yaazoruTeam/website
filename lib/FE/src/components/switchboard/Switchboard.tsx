b37e54ddfdee1a5e63d7157fbcb481e80b7fdf9b
michal22362 <michal22362@gmail.com>
2025-05-14 17:36:37 +0300

    ✨ Add a general button - return to previous page

--- lib/FE/src/components/switchboard/Switchboard.tsx
+++ lib/FE/src/components/switchboard/Switchboard.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import ChangeAccountModal from "./ChangeAccountModal";
+import ReturnButton from "../designComponent/ReturnButton";
 
 const Switchboard: React.FC = () => {
   const [open, setOpen] = useState(false);
@@ -8,6 +9,7 @@ const Switchboard: React.FC = () => {
     <>
       <button onClick={() => setOpen(true)}>פתח פופ-אפ</button>
       <ChangeAccountModal open={open} onClose={() => setOpen(false)} />
+      <ReturnButton />
     </>
   );
 };
