2a1d27f07b123ef315e8be0f501984d47f56d356
michal22362 <michal22362@gmail.com>
2025-05-14 09:04:44 +0300

    ✨ Adding a ChangeAccountModal pop-up componentAdding a ChangeAccountModal pop-up component

--- lib/FE/src/components/switchboard/Switchboard.tsx
+++ lib/FE/src/components/switchboard/Switchboard.tsx
@@ -1,6 +1,14 @@
+import React, { useState } from "react";
+import ChangeAccountModal from "./ChangeAccountModal";
+
 const Switchboard: React.FC = () => {
+  const [open, setOpen] = useState(false);
+
   return (
-    <></>
+    <>
+      <button onClick={() => setOpen(true)}>פתח פופ-אפ</button>
+      <ChangeAccountModal open={open} onClose={() => setOpen(false)} />
+    </>
   );
 };
 
