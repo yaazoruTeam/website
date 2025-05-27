59fbab88ad44d9bdcc15871bb4c1cfdbd5d69143
michal22362 <michal22362@gmail.com>
2025-05-27 17:18:43 +0300

    🐛 Remove unused useState import from StatusCard component

--- lib/FE/src/components/designComponent/StatusCard.tsx
+++ lib/FE/src/components/designComponent/StatusCard.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import { Box } from "@mui/material";
 import { colors } from "../../styles/theme";
 import { ChevronDownIcon } from "@heroicons/react/24/outline";
