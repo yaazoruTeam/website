66f3db3482ac2aa0086d8fa600e8f95d41de2f5a
Efrat Grinboim <151626406+Efrat6821@users.noreply.github.com>
2025-06-08 13:36:11 +0300

    🗑️ Remove unnecessary comment from CallLog component

--- lib/FE/src/components/switchboard/CallLog.tsx
+++ lib/FE/src/components/switchboard/CallLog.tsx
@@ -72,7 +72,6 @@ const CallLog: React.FC = () => {
                         onClick={refresh}
                     />
                 </Box>
-                //
             </Box>
         </Box>
     );
