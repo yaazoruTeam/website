fa0d1df2a9a20ad6c5478c15362c09bd5149945b
michal22362 <michal22362@gmail.com>
2025-07-09 14:29:43 +0300

    style: improve code formatting and consistency in CallLog component

--- lib/FE/src/components/switchboard/CallLog.tsx
+++ lib/FE/src/components/switchboard/CallLog.tsx
@@ -1,134 +1,132 @@
-import { useState } from "react";
-import { Box } from "@mui/material";
-import CustomTypography from "../designComponent/Typography";
-import { useTranslation } from "react-i18next";
-import { useParams } from "react-router-dom";
-import { CustomButton } from "../designComponent/Button";
-import { ArrowDownOnSquareIcon, ArrowPathIcon } from "@heroicons/react/24/outline";
-import PhoneNumber from "./PhoneNumer";
-import { formatDateToString } from "../designComponent/FormatDate";
-import CustomTable from "../designComponent/CustomTable";
+import { useState } from 'react'
+import { Box } from '@mui/material'
+import CustomTypography from '../designComponent/Typography'
+import { useTranslation } from 'react-i18next'
+import { useParams } from 'react-router-dom'
+import { CustomButton } from '../designComponent/Button'
+import { ArrowDownOnSquareIcon, ArrowPathIcon } from '@heroicons/react/24/outline'
+import PhoneNumber from './PhoneNumer'
+import { formatDateToString } from '../designComponent/FormatDate'
+import CustomTable from '../designComponent/CustomTable'
 
 const CallLog: React.FC = () => {
-    const { t } = useTranslation();
-    const { callId } = useParams();
-    const [calls, setCalls] = useState<any[]>([//to do:Change the data type to match the actual data.
-        {
-            country: '972-79-606-4286',
-            target: '1-973-964-0286',
-            date: Date.now(),
-            durationCall: '01:04:23',
-            timeCall: '23:44:00 pm',
-            costInShekels: '03.00',
-            penniesPerMinute: '04.00'
-        }
-    ]);
+  const { t } = useTranslation()
+  const { callId } = useParams()
+  const [calls, setCalls] = useState<any[]>([
+    //to do:Change the data type to match the actual data.
+    {
+      country: '972-79-606-4286',
+      target: '1-973-964-0286',
+      date: Date.now(),
+      durationCall: '01:04:23',
+      timeCall: '23:44:00 pm',
+      costInShekels: '03.00',
+      penniesPerMinute: '04.00',
+    },
+  ])
 
   //to to:useEffect to fetch the data from the server
 
-    const columns = [
-        { label: t('country'), key: 'country' },
-        { label: t('target'), key: 'target' },
-        { label: t('date'), key: 'date' },
-        { label: t('durationCall'), key: 'durationCall' },
-        { label: t('timeCall'), key: 'timeCall' },
-        { label: t('costInShekels'), key: 'costInShekels' },
-        { label: t('penniesPerMinute'), key: 'penniesPerMinute' },
-    ];
+  const columns = [
+    { label: t('country'), key: 'country' },
+    { label: t('target'), key: 'target' },
+    { label: t('date'), key: 'date' },
+    { label: t('durationCall'), key: 'durationCall' },
+    { label: t('timeCall'), key: 'timeCall' },
+    { label: t('costInShekels'), key: 'costInShekels' },
+    { label: t('penniesPerMinute'), key: 'penniesPerMinute' },
+  ]
 
-    const tableData = calls.map(call => ({
-        country: <PhoneNumber country="Israel" phoneNumber={call.country} />,
-        target: <PhoneNumber country="USA" phoneNumber={call.target} />,
-        date: formatDateToString(call.date),
-        durationCall: call.durationCall,
-        timeCall: call.timeCall,
-        costInShekels: call.costInShekels,
-        penniesPerMinute: call.penniesPerMinute,
-    }));
+  const tableData = calls.map((call) => ({
+    country: <PhoneNumber country='Israel' phoneNumber={call.country} />,
+    target: <PhoneNumber country='USA' phoneNumber={call.target} />,
+    date: formatDateToString(call.date),
+    durationCall: call.durationCall,
+    timeCall: call.timeCall,
+    costInShekels: call.costInShekels,
+    penniesPerMinute: call.penniesPerMinute,
+  }))
 
-    console.log('callId', callId);
+  console.log('callId', callId)
 
-    const downloadFile = () => {
-        //to do: download data to file
-        console.log('Downloading file...');
+  const downloadFile = () => {
+    //to do: download data to file
+    console.log('Downloading file...')
+  }
 
-    };
+  const refresh = () => {
+    //to do: refresh list - Re-fetch the data - send to useEffect again
+    console.log('Refreshing...')
+    setCalls([
+      //to do: Change this to fetch the actual data from the server
+      {
+        country: '972-79-606-4286',
+        target: '1-973-964-0286',
+        date: Date.now(),
+        durationCall: '01:04:23',
+        timeCall: '23:44:00 pm',
+        costInShekels: '03.00',
+        penniesPerMinute: '04.00',
+      },
+    ])
+  }
 
-    const refresh = () => {
-        //to do: refresh list - Re-fetch the data - send to useEffect again
-        console.log('Refreshing...');
-        setCalls([ //to do: Change this to fetch the actual data from the server
-            {
-                country: '972-79-606-4286',
-                target: '1-973-964-0286',
-                date: Date.now(),
-                durationCall: '01:04:23',
-                timeCall: '23:44:00 pm',
-                costInShekels: '03.00',
-                penniesPerMinute: '04.00'
-            }
-        ]);
-    };
-
-    return (
-        <Box sx={{
-            paddingLeft: '10%',
-            paddingRight: '15%',
-        }}>
-            <Box sx={{
-                display: 'flex',
-                justifyContent: 'space-between',
-                alignItems: 'center',
-            }}>
-                <Box>
-                    <Box sx={{
-                        display: 'flex',
-                        alignItems: 'end',
-                        gap: '10px',
-                    }}>
-                        <CustomTypography
-                            variant="h1"
-                            weight="bold"
-                            text={`${t('callLog')} /`}
-                        />
-                        <CustomTypography
-                            variant="h2"
-                            weight="regular"
-                            text={`${callId}`}
-                        />
-                    </Box>
-                    {/*to do : לבדוק מאיפה מגיעים הנתונים של זה ולשנות את זה לנתונים האמיתיים אחרי קראית שרת.*/}
-                    <CustomTypography
-                        variant="h3"
-                        weight="regular"
-                        text={`${t('total')} 146 ${t('calls')} ${t('during')} 06:32:42 ${t('hours')} $3.765`}
-                    />
-                </Box>
-                <Box sx={{
-                    display: 'flex',
-                    alignItems: 'center',
-                    gap: '16px',
-                }}>
-                    <CustomButton
-                        label={t('downloadingFile')}
-                        icon={<ArrowDownOnSquareIcon />}
-                        sx={{ flexDirection: 'row-reverse' }}
-                        onClick={downloadFile}
-                    />
-                    <CustomButton
-                        label={t('refresh')}
-                        icon={<ArrowPathIcon />}
-                        sx={{ flexDirection: 'row-reverse' }}
-                        onClick={refresh}
-                    />
-                </Box>
-            </Box>
-            <CustomTable
-                columns={columns}
-                data={tableData}
-            />
+  return (
+    <Box
+      sx={{
+        paddingLeft: '10%',
+        paddingRight: '15%',
+      }}
+    >
+      <Box
+        sx={{
+          display: 'flex',
+          justifyContent: 'space-between',
+          alignItems: 'center',
+        }}
+      >
+        <Box>
+          <Box
+            sx={{
+              display: 'flex',
+              alignItems: 'end',
+              gap: '10px',
+            }}
+          >
+            <CustomTypography variant='h1' weight='bold' text={`${t('callLog')} /`} />
+            <CustomTypography variant='h2' weight='regular' text={`${callId}`} />
+          </Box>
+          {/*to do : לבדוק מאיפה מגיעים הנתונים של זה ולשנות את זה לנתונים האמיתיים אחרי קראית שרת.*/}
+          <CustomTypography
+            variant='h3'
+            weight='regular'
+            text={`${t('total')} 146 ${t('calls')} ${t('during')} 06:32:42 ${t('hours')} $3.765`}
+          />
         </Box>
-    );
+        <Box
+          sx={{
+            display: 'flex',
+            alignItems: 'center',
+            gap: '16px',
+          }}
+        >
+          <CustomButton
+            label={t('downloadingFile')}
+            icon={<ArrowDownOnSquareIcon />}
+            sx={{ flexDirection: 'row-reverse' }}
+            onClick={downloadFile}
+          />
+          <CustomButton
+            label={t('refresh')}
+            icon={<ArrowPathIcon />}
+            sx={{ flexDirection: 'row-reverse' }}
+            onClick={refresh}
+          />
+        </Box>
+      </Box>
+      <CustomTable columns={columns} data={tableData} />
+    </Box>
+  )
 }
 
-export default CallLog;
+export default CallLog
