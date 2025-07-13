7ce4f9be71de68b9d884896b60980aa53aaa7e03
Efrat Grinboim <151626406+Efrat6821@users.noreply.github.com>
2025-07-13 11:42:37 +0300

    ♻️ Refactor: remove WidelyDetails component from Devices and simplify return structure

--- lib/FE/src/components/devices/devices.tsx
+++ lib/FE/src/components/devices/devices.tsx
@@ -1,10 +1,9 @@
 import AppBar from '@mui/material/AppBar'
-import WidelyDetails from './widelyDetails'
 
 const Devices = () => {
-  return <>
-  <WidelyDetails simNumber={'89972123300003347812'} />
-  <AppBar sx={{ display: 'flex' }}></AppBar></>
+  return (
+    <AppBar sx={{ display: 'flex' }}></AppBar>
+  )
 }
 
 export default Devices
