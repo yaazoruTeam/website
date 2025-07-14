1b9199d1b0ee437623654640bea6d9b5c344620d
Efrat Grinboim <151626406+Efrat6821@users.noreply.github.com>
2025-07-14 13:07:19 +0300

    ✨ Add: implement terminateMobile function and corresponding router endpoint

--- lib/backend/src/controller/widely.ts
+++ lib/backend/src/controller/widely.ts
@@ -200,7 +200,23 @@ const getAllUserData = async (req: Request, res: Response, next: NextFunction): 
     }
 }
 
-export { searchUsers, getMobiles, getMobileInfo, getAllUserData }
+//to do: Move this function into the correct folder and file.
+const terminateMobile = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+    try {
+        const { endpoint_id } = req.body
+        validateRequiredParam(endpoint_id, 'endpoint_id')
+
+        // const userData = await searchUsersData(simNumber)
+        const result: Widely.Model = await callingWidely(
+            'prov_terminate_mobile',
+            { endpoint_id: endpoint_id }
+        )
+        res.status(result.error_code).json(result)
+    } catch (error: any) {
+        next(error)
+    }
+}
+export { searchUsers, getMobiles, getMobileInfo, getAllUserData, terminateMobile }
 
 
                                         
